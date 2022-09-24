5f41937a2c4b4ca33b850836473cf5233c748055
Zain Fathoni <me@zainf.dev>
2022-09-24 12:18:37 +0800

    feat: implement view contribution page

--- app/routes/contributions/$activityId.tsx
+++ app/routes/contributions/$activityId.tsx
@@ -1,7 +1,9 @@
 import { Activity } from '@prisma/client'
 import { json, LoaderFunction, redirect } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import { Card, CardContainer } from '~/components/card'
 import { getActivityById } from '~/model/activities'
+import { getCategoryByCategorySlug } from '~/model/categories'
 
 export const loader: LoaderFunction = async ({ params }) => {
   const { activityId } = params
@@ -16,6 +18,33 @@ export const loader: LoaderFunction = async ({ params }) => {
 }
 
 export default function ViewContribution() {
-  const { activity } = useLoaderData<{ activity: Activity }>()
-  return <div>{JSON.stringify(activity)}</div>
+  const { activity } = useLoaderData<{
+    activity: Activity & {
+      category: {
+        title: string
+        slug: string
+      }
+    }
+  }>()
+  const { icon, foregroundColor, backgroundColor } = getCategoryByCategorySlug(
+    activity.category.slug
+  )
+  return (
+    <CardContainer>
+      <Card
+        name={activity.name}
+        cta="Kunjungi"
+        link={activity.url}
+        secondaryCta="Koreksi"
+        secondaryLink="#"
+        description={activity.description}
+        category={activity.category.title}
+        categorySlug={activity.category.slug}
+        icon={icon}
+        foregroundColor={foregroundColor}
+        backgroundColor={backgroundColor}
+      />
+      )
+    </CardContainer>
+  )
 }
