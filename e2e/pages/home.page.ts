c620052ca91aa3e288e65cf082a477292c0b6857
depapp <depa.article@gmail.com>
2022-10-13 14:39:55 +0700

    feat: e2e test using POM pattern

---
+++ e2e/pages/home.page.ts
@@ -0,0 +1,21 @@
+import { Page, Locator, expect } from '@playwright/test';
+
+export class HomePage {
+  readonly page: Page;
+  readonly activityCard: Locator;
+  readonly paudActivity: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.activityCard = page.locator('h3.text-lg.font-medium');
+    this.paudActivity = page.locator('text=PAUDPendidikan Anak Usia Dini >> span')
+  }
+
+  async open() {
+    await this.page.goto('/');
+  }
+
+  async goToPaudActivity() {
+    await this.paudActivity.click();
+  }
+}
