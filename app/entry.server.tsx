cb46968fb7dcfd9c20ac8dec5f5690cd3dcbc4c8
Zain Fathoni <me@zainf.dev>
2023-02-09 00:43:00 +0800

    feat(remix): serve bots accordingly
    References:
    - https://remix.run/docs/en/v1/guides/streaming
    - https://github.com/remix-run/indie-stack/blob/cec1efe9e1faa678e8476b9105a8174cefb5800b/app/entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,6 +3,9 @@ import type { EntryContext } from '@remix-run/node'
 import { Response } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
 import { renderToPipeableStream } from 'react-dom/server'
+import isbot from 'isbot'
+
+const ABORT_DELAY = 5000
 
 export default function handleRequest(
   request: Request,
@@ -10,26 +13,41 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  responseHeaders.set('Content-Type', 'text/html')
+  const callbackName = isbot(request.headers.get('user-agent'))
+    ? 'onAllReady'
+    : 'onShellReady'
 
-  return new Promise((resolve) => {
-    const { pipe } = renderToPipeableStream(
+  return new Promise((resolve, reject) => {
+    let didError = false
+
+    const { pipe, abort } = renderToPipeableStream(
       <RemixServer context={remixContext} url={request.url} />,
       {
-        onShellReady() {
+        [callbackName]: () => {
           const body = new PassThrough()
 
           responseHeaders.set('Content-Type', 'text/html')
 
           resolve(
             new Response(body, {
-              status: responseStatusCode,
+              status: didError ? 500 : responseStatusCode,
               headers: responseHeaders,
             })
           )
+
           pipe(body)
+        },
+        onShellError: (err: unknown) => {
+          reject(err)
+        },
+        onError: (error: unknown) => {
+          didError = true
+
+          console.error(error)
         },
       }
     )
+
+    setTimeout(abort, ABORT_DELAY)
   })
 }
