3a49f7ec74458c46ae2ce3b0385980de0f2016b6
Zain Fathoni <me@zainf.dev>
2023-02-11 01:51:27 +0700

    test(e2e): test contributions-list page

---
+++ e2e/contributions-list.spec.ts
@@ -0,0 +1,13 @@
+import { test } from './base-test'
+import { activities } from '~/model/activities'
+
+const { expect } = test
+
+test('Viewing contributions list', async ({ page, queries: { getByText } }) => {
+  const [{ nama, ringkasan }] = activities
+
+  await page.goto('/contributions')
+
+  await expect(await getByText(nama)).toBeVisible()
+  await expect(await getByText(ringkasan)).toBeVisible()
+})
